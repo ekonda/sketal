7eb57bf368afbc113b57b15eb8398ab3d10ea97e
Oleg Parfilov <oleg-doktor@mail.ru>
2016-06-16 14:20:17 +0300

    Правки сообщений в greeting.py

--- plugins/greeting.py
+++ plugins/greeting.py
@@ -20,10 +20,10 @@ class Plugin:
     def call(self, msg):
         greetings = []
 
-        greetings.append(u'Я - чатбот')
+        greetings.append(u'Доброго времени суток.')
         greetings.append(u'Кекеке')
-        greetings.append(u'Запущен и готов служить')
-        greetings.append(u'У контакта ужасный флуд-контроль, %username%')
-        greetings.append(u'Хуяк-хуяк и в продакшен')
+        greetings.append(u'Запущен и готов служить.')
+        greetings.append(u'У контакта ужасный флуд-контроль, %username%.')
+        greetings.append(u'Хуяк-хуяк и в продакшен!')
 
         self.vk.respond(msg, {'message': random.choice(greetings)})
