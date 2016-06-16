c86a97827389b4044530960819b549b89486b75e
Oleg Parfilov <oleg-doktor@mail.ru>
2016-06-16 18:38:37 +0300

    Уменьшил offset

--- plugins/boobs.py
+++ plugins/boobs.py
@@ -30,7 +30,7 @@ class Plugin:
             values = {
             # owner_id = ид группы
                 'owner_id': -20282193,
-                'offset': random.randint(1, 1200),
+                'offset': random.randint(1, 500),
                 'count': 1
             }
 
