6d28b5be4f1bacc47c61a2c27ed7292809e370d9
Oleg Parfilov <oleg-doktor@mail.ru>
2016-06-16 19:44:14 +0300

    Update loader.sh

--- loader.sh
+++ loader.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# http://stackoverflow.com/questions/696839/how-do-i-write-a-bash-script-to-restart-a-process-if-it-dies
 
 until python lolbot.py; do
     echo "Server 'myserver' crashed with exit code $?.  Respawning.." >&2
