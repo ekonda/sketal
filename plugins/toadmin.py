0aec5a1503eada6cd1e0fdf488563e63e89907ea
DarkCppCoder <pigarev.sevastyan@gmail.com>
2017-04-29 21:06:10 +0700

    Large grammar fixes, new plugin

---
+++ plugins/toadmin.py
@@ -0,0 +1,27 @@
+from plugin_system import Plugin
+from settings import ADMINS
+plugin = Plugin('Сообщение админу', usage='админу [текст] - отослать администрации сообщение');
+
+@plugin.on_init()
+def init(vk):
+    if "some_data" not in plugin.data:
+        plugin.data["some_data"] = {}
+
+@plugin.on_command('админу')
+async def toadmin(msg, args):
+    data = ' '.join(args)
+    sender_data = await msg.vk.method('users.get', {'user_ids': msg.id, 'name_case': "gen"})
+    sender_data = sender_data[0]
+    for uid in ADMINS:
+        val = {
+            'peer_id': uid,
+            'message': f"Сообщение от {sender_data['first_name']} {sender_data['last_name']}!\n\"{data}\"",
+        }
+
+        if "attach1" in msg.brief_attaches:
+            val['attachment'] = ",".join(str(x) for x in await msg.full_attaches)
+
+        result = await msg.vk.method('messages.send', val)
+    if not result:
+        return await msg.answer('Сообщение не удалось отправить!')
+    await msg.answer('Сообщение успешно отправлено!')
